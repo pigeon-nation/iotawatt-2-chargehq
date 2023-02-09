43d487cea402e040226f2fdb134efd004e97d241
Darek Margas <71180450+darek-margas@users.noreply.github.com>
2023-02-09 14:01:45 +1100

    Moved configuration into arguments

--- iotawatt2chargehq.py
+++ iotawatt2chargehq.py
@@ -3,28 +3,32 @@ import sys, os
 import locale
 import json
 import requests
+import argparse
+import logging
+import time
 
 __author__ = "Darek Margas"
 __copyright__ = "Copyright 2023, Darek Margas"
 __license__ = "MIT"
 
-# IOTAWATT
-ip="192.168.0.1"
+# Arguments
+my_parser = argparse.ArgumentParser(description="Update ChargeHQ from IoTaWatt")
+my_parser.add_argument( "--ip", metavar="IP address", type=str, help="IP Address of IoTaWatt" )
+my_parser.add_argument( "--grid", metavar="Net grid import", type=str, help="Net import from Grid (kW)")
+my_parser.add_argument( "--production", metavar="PV production", type=str, help="PV production (kW)")
+my_parser.add_argument( "--key", metavar="API key", type=str, help="ChargeHQ API key")
 
-# Names of inputs/outputs
-IO_NET_IMPORT_KW = "Grid"
-IO_PRODUCTION_KW = "Inverter"
+args = my_parser.parse_args()
 
 # Charge HQ
 chq_url = 'https://api.chargehq.net/api/public/push-solar-data'
-api_key = "your key"
 
 # Getting data
-g = requests.get( "http://{}/query".format(ip), params = "select=[{}.watts,{}.watts]&begin=s-1m&end=s&group=all&header=no".format(IO_NET_IMPORT_KW, IO_PRODUCTION_KW), timeout=15)
+g = requests.get( "http://{}/query".format(args.ip), params = "select=[{}.watts,{}.watts]&begin=s-1m&end=s&group=all&header=no".format(args.grid, args.production), timeout=15)
 
 #Assembling json
 data = {}
-data['apiKey'] = api_key
+data['apiKey'] = args.key
 data['siteMeters'] = {}
 data['siteMeters']['production_kw'] = round( g.json()[0][1] / 1000, 3)
 data['siteMeters']['net_import_kw'] = round( g.json()[0][0] / 1000, 3)
@@ -35,4 +39,7 @@ payload = json.dumps(data)
 header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 p = requests.post( chq_url, data=payload, headers=header)
+
 #print(payload, p.text)
+logging.info(payload, p.text)
+
